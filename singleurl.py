44d6164a03186026f8a70e7f7c1dfcaa9a3615e2
Robbie Corley <musicmancorley@gmail.com>
2017-01-09 13:39:22 -0500

    Update singleurl.py

--- singleurl.py
+++ singleurl.py
@@ -3,8 +3,8 @@ import urllib2
 import requests
 
 theresponse=0
-machineprefix="http://10.32.23."
-securemachineprefix="https://10.32.23."
+machineprefix="http://192.168.1."
+securemachineprefix="https://192.168.1."
 #add 1 to make it 1-254
 for i in range(20,21):
     try:
@@ -15,5 +15,5 @@ for i in range(20,21):
 
         raw_input('Press any key to continue with the scan...')
     except:
-        print "http://10.32.23."+str(i)
+        print "http://192.168.1."+str(i)
         print "no HVAC machines here..."
